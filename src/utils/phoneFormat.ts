a806260eb5540ad4b9de8e1a6b15af09db1f93d5
tohird03 <tohirjondoniyorov@gmail.com>
2026-04-21 09:48:17 +0500

    Finished bugs

--- src/utils/phoneFormat.ts
+++ src/utils/phoneFormat.ts
@@ -1,5 +1,9 @@
-export const formatPhoneNumber = (phoneNumber: string) => {
-  const formattedNumber = phoneNumber.replace(
+export const formatPhoneNumber = (phoneNumber?: string | number | null) => {
+  if (!phoneNumber) return '998000000000';
+
+  const cleaned = String(phoneNumber);
+
+  const formattedNumber = cleaned.replace(
     /^(\d{3})(\d{2})(\d{3})(\d{2})(\d{2})$/,
     '$1 $2 $3 $4 $5'
   );
